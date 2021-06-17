de9979152e243291ace4aaac165e4b7a7a981ca5
mxr <eetu.kivelae@gmail.com>
2021-06-17 20:50:52 +0300

    Feature: Added huuttis command

--- src/commands/huuttis.go
+++ src/commands/huuttis.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"log"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -10,10 +13,33 @@ var huuttisInfo = discordgo.ApplicationCommand{
 }
 
 func huuttisCmd(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	channel := i.ChannelID
+
+	file, err := os.Open("assets/huuttis.mp4")
+	if err != nil {
+		return
+	}
+
+	v := discordgo.File{
+		Name:        "huuttis.mp4",
+		ContentType: "video/mp4",
+		Reader:      file,
+	}
+
+	s.ChannelMessageSendComplex(channel, &discordgo.MessageSend{
+		Files: []*discordgo.File{
+			&v,
+		},
+	})
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionApplicationCommandResponseData{
 			Content: "huuttis mint",
 		},
 	})
+
+	if err != nil {
+		log.Printf("Huuttis issue: %v", err)
+	}
 }
